f8c363c6cff4fcc32d8d103e3d6ded4056f0bf78
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-11-13 16:33:10 +0300

    Add docstrings to test functions

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -11,12 +11,14 @@ from tests.common import MakefileTestCase, slow_test
 class TestInvocation(MakefileTestCase):
 
     def test_gnu_make(self):
+        '''Only GNU Make is supported'''
         make = self.make('--version')
         self.check_returncode(make)
         self.assertTrue('gnu make' in make.stdout.decode(sys.stdout.encoding).lower())
 
     @slow_test
     def test_creating(self):
+        '''Create empty virtual environment'''
         make = self.make('venv')
         self.check_returncode(make)
         self.assertTrue(os.path.isdir(os.path.join(self.tmpdir.name, '.venv')))
