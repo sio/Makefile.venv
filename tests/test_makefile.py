d90f0e1a39d04a2a46e0236264b9c02d35345409
Vitaly Potyarkin <sio.wtf@gmail.com>
2019-11-13 17:22:32 +0300

    Simple tests for show-venv and clean-venv

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -19,3 +19,11 @@ class TestInvocation(MakefileTestCase):
         '''Create empty virtual environment'''
         make = self.make('venv')
         self.assertTrue(os.path.isdir(os.path.join(self.tmpdir.name, '.venv')))
+
+        version = self.make('show-venv')
+        for line in ('python ', 'pip ', 'venv: '):
+            with self.subTest(line=line):
+                self.assertIn(line.lower(), version.stdout.lower())
+
+        self.make('clean-venv')
+        self.assertFalse(os.path.isdir(os.path.join(self.tmpdir.name, '.venv')))
