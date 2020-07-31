68822de247277f92acae814ba04fd558dae3077c
Vitaly Potyarkin <sio.wtf@gmail.com>
2020-07-31 15:15:53 +0300

    Add tests for issue #8
    Check that:
    - REQUIREMENTS_TXT can be generated with a Makefile recipe
    - Custom modifications can be performed in the virtual environment during
      the first Makefile.venv pass

---
+++ tests/data/pip-compile.mk
@@ -0,0 +1,33 @@
+all: venv
+	$(VENV)/pyflakes --help
+
+
+clean: clean-venv
+	-$(RM) Makefile.venv requirements.txt
+
+
+# You need to explicitly specify this value because by default it's ok for
+# requirements.txt to be missing. This statement MUST come before `include
+# Makefile.venv`
+REQUIREMENTS_TXT=requirements.txt  requirements.txt   # add trailing whitespace
+
+
+# Save pip-compile path to variable for brevity
+PIP_COMPILE=$(VENV)/pip-compile$(EXE)
+
+
+include {{ Makefile.venv }}
+
+
+# You need to inject pip-compile into virtual environment
+# before Makefile.venv finishes working on it, but after venv is created.
+# There already exists a target you can add as dependency for this case:
+$(PIP_COMPILE): | $(VENV)
+	$(VENV)/pip install pip-tools  # or whatever method you prefer
+	touch $@
+
+
+# Your requirements file directly depends upon *.in file and also requires
+# pip-compile to be available
+requirements.txt: requirements.in | $(PIP_COMPILE)
+	$(PIP_COMPILE) --output-file $@ $<
---
+++ tests/test_pip_compile.py
@@ -0,0 +1,31 @@
+'''
+Tests for issue #8
+https://github.com/sio/Makefile.venv/issues/8
+
+Support for generated requirements.txt files
+'''
+
+
+from tests.common import MakefileTestCase, slow_test
+
+
+class TestPipCompile(MakefileTestCase):
+
+    @slow_test
+    def test_issue8(self):
+        '''Check that REQUIREMENTS_TXT can be generated with a Makefile recipe'''
+        self.copy_data('requirements.in')
+        makefile = self.copy_data('pip-compile.mk', makefile=True)
+
+        make = self.make(makefile=makefile)
+        for line in [
+                'Collecting pyflakes',
+                'show this help message and exit',
+                'usage: pyflakes',
+        ]:
+            self.assertIn(line, make.stdout)
+
+        # Second invokation must not need to rebuild venv
+        repeat = self.make(makefile=makefile)
+        self.assertIn('pyflakes --help', repeat.stdout.splitlines()[1])
+
