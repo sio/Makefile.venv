938f7d55186cc7dfbc7e12e50ce0a08a02e1f977
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-07-20 15:26:33 +0300

    Add tests for "make -R"

--- tests/test_makefile.py
+++ tests/test_makefile.py
@@ -15,15 +15,26 @@ class TestInvocation(MakefileTestCase):
         self.assertTrue('gnu make' in make.stdout.lower())
 
     @slow_test
-    def test_creating(self):
+    def test_creating(self, *cli_args):
         '''Create empty virtual environment'''
-        make = self.make('debug-venv', 'venv')
+        make = self.make('debug-venv', 'venv', *cli_args)
         self.assertTrue(os.path.isdir(os.path.join(self.tmpdir.name, '.venv')))
 
-        version = self.make('show-venv')
+        version = self.make('show-venv', *cli_args)
         for line in ('python ', 'pip ', 'venv: '):
             with self.subTest(line=line):
                 self.assertIn(line.lower(), version.stdout.lower())
 
-        self.make('clean-venv')
+        self.make('clean-venv', *cli_args)
         self.assertFalse(os.path.isdir(os.path.join(self.tmpdir.name, '.venv')))
+
+    @slow_test
+    def test_no_builtin_variables(self):
+        '''
+        Check that "make -R" does not break anything
+
+        Thanks to @martinthomson:
+            https://github.com/sio/Makefile.venv/issues/19
+            https://github.com/sio/Makefile.venv/pull/20
+        '''
+        return self.test_creating('-R')
